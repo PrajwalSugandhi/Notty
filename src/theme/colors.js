0cdbf13715a72c7002c6e05b72b7047397dd7104
Prajwal <prajwal@HCPL-MAC-129.local>
2025-05-21 23:43:15 +0530

    feat: Added assets

---
+++ src/theme/colors.js
@@ -0,0 +1,15 @@
+const colors = {
+    electricViolet: '#791FF8',
+    heliotrope: '#BC8FFB',
+    fog: '#E4D2FE',
+    magnolia: '#F8F4FF',
+    mobster: '#878090',
+    graySuit: '#C3BFC8',
+    mischka: '#E7E6E9',
+    blackRussian: '#0E0121',
+    mulledWine: '#584E66',
+    white: '#FDFDFD'
+}
+
+
+export default colors;
---
+++ src/theme/fonts.js
@@ -0,0 +1,8 @@
+const fonts = {
+    PoppinsBold: 'Poppins-Bold',
+    PoppinsSemiBold: 'Poppins-SemiBold',
+    PoppinsMedium: 'Poppins-Medium',
+    PoppinsRegular: 'Poppins-Regular',
+}
+
+export default fonts;
